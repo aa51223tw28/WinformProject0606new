11e93f2ee6ccc2551ae757a9758151287c837509
user <user@MSI>
2023-06-06 23:18:26 +0800

    加入專案檔案。

---
+++ WinformPoject0527/1-10BuyerReceiveStatus.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinformPoject0527.EFModels;
+
+namespace WinformPoject0527
+{
+    public partial class BuyerReceiveStatus : Form
+    {
+        
+        public int _shipid;
+        public int _sellerid;
+        public int _orderIDBuyerReceiveStatus;
+
+
+        List<BuyerShipDto> data;
+
+        public BuyerReceiveStatus(int shipid, int sellerid)
+        {
+            InitializeComponent();
+            _shipid = shipid;
+            _sellerid = sellerid;
+            this.Load += BuyerReceiveStatus_Load;
+           
+        }
+
+        private void BuyerReceiveStatus_Load(object sender, EventArgs e)
+        {
+            textBoxShipmentID.Text = _shipid.ToString();
+
+            //datagridview
+            var db = new AppDbContext();
+
+
+            var query = db.OrderDetails
+                            .Where(x => x.OrderID == _orderIDBuyerReceiveStatus)
+                            .OrderBy(x => x.ProductID)
+                            .Join(db.Products, X => X.ProductID, y => y.ProductID, (x, y) => new
+                            {
+                                _productID = x.ProductID,
+                                _productname = y.ProductName,
+                                _productQuantity = x.Quantity,
+                                _sellerid=y.SellerID
+                            })
+                            .Join(db.Shipments, x => x._sellerid, y => y.SellerID, (x, y) => new
+                            {
+                                _productIDs=x._productID,
+                                _productnames=x._productname,
+                                _productQuantitys=x._productQuantity,
+                                _shipids=y.ShipmentID
+                            })
+                            .Where(y=>y._shipids==_shipid)
+                            .ToList();
+
+            data = query.Select(x => new BuyerShipDto()
+            {
+                ProductId = x._productIDs,
+                ProductName = x._productnames,
+                Quantity = x._productQuantitys
+
+            }).ToList();
+            this.dataGridView1.DataSource = data;
+
+
+        }
+    }
+}
---
+++ WinformPoject0527/1-11Buyerpurchasehistory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinformPoject0527.EFModels;
+
+namespace WinformPoject0527
+{
+    public partial class Buyerpurchasehistory : Form
+    {
+        public int _userIdBuyerpurchasehistory;
+        public int _cartIdBuyerpurchasehistory;
+
+        List<ListAllDto> data;
+
+
+
+
+        public Buyerpurchasehistory()
+        {
+            InitializeComponent();
+            this.Load += Buyerpurchasehistory_Load;
+        }
+
+        private void Buyerpurchasehistory_Load(object sender, EventArgs e)
+        {
+            var db=new AppDbContext();
+            var acc=db.Users.Where(x=>x.UserID==_userIdBuyerpurchasehistory)
+                                .Select(x=>x.UserAccount).FirstOrDefault();
+
+            textBoxaccount.Text = acc;
+
+            //datagridview
+            var query =db.Orders.Where(x=>x.UserID== _userIdBuyerpurchasehistory)                                
+                                .ToList();
+
+            data=query.Select(x=>new ListAllDto()
+            {
+                OrderID=x.OrderID,
+                ShipmentDate= (DateTime)x.OrderDate
+
+            }).ToList();
+
+            dataGridView1.DataSource= data;
+           
+        }
+
+        private void buttonbackmain_Click(object sender, EventArgs e)
+        {
+            var frm = new BuyerMain();
+            frm.MdiParent = Login.ActiveForm;
+            frm._userIdMain = _userIdBuyerpurchasehistory;
+            frm._cartIdBuyerMain = _cartIdBuyerpurchasehistory;
+            frm.Show();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex<0) return;
+
+            int _orderid = this.data[e.RowIndex].OrderID;
+
+            var frm = new BuyerListEnd(_orderid);
+            frm.MdiParent = Login.ActiveForm;
+            //frm._orderID= _orderid;
+            frm._userIdBuyerListEnd= _userIdBuyerpurchasehistory;
+            frm._cartIdBuyerListEnd = _cartIdBuyerpurchasehistory;
+            frm.Show();
+        }
+    }
+}
